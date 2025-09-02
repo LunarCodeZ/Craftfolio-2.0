fbe7f0982f59663663a14bca56fc23533af16706
LunarCodeZ <erik.atatawiyu@gmail.com>
2025-09-02 21:27:31 +0700

    Initial commit

---
+++ plan/design-script.js
@@ -0,0 +1,12 @@
+var menuButton = 1;
+
+function cycleButton() {
+    menuButton += 1;
+
+    if (menuButton === 1) {
+        document.getElementById("navbar-menus").style.display = "none";
+    } else {
+        menuButton *= 0;
+        document.getElementById("navbar-menus").style.display = "flex";
+    }
+}
